ff8c0b3316633c196a1ac88e40a28665219eedb0
SOLID <solidbiker3000@gmail.com>
2025-07-24 16:24:03 -0500

    LayerControls pt.2
    (all done!)

--- cnn-builder/src/components/LayerControls.tsx
+++ cnn-builder/src/components/LayerControls.tsx
@@ -28,6 +28,58 @@ const LayerControls: React.FC<LayerControlsProps> = ({ onAdd }) => {
 
     const handleParamChange = (e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
         const { name, value } = e.target;
-        setParams((prev) => ({ ...prev, })) //BREH. MY MOMS FORCING ME TO SLEEP. SMH. WILL FINISH TMRW.
-    }
-}
+        setParams((prev) => ({ ...prev, [name]: isNaN(Number(value)) ? value: Number(value)}));
+    };
+
+    const handleAdd = () => {
+        if (params.type) {
+            onAdd(params as CNNLayer);
+        }
+    };
+
+    const renderParams = () => {
+        switch (layerType) {
+            case 'Conv2D':
+                return (
+                    <>
+                        <label>Filters: <input name="filters" type="number" value={(params as any).filters} onChange={handleParamChange}/></label>
+                        <label>Kernel Size: <input name="kernelSize" type="number" value={(params as any).kernelSize} onChange = {handleParamChange}/></label>
+                        <label>Strides: <input name="strides" type="number" value={(params as any).strides} onchange={handleParamChange}/></label>
+                    </>
+                );
+            case 'MaxPooling2D':
+                return (
+                    <>
+                        <label>Pool Size: <input name="poolSize" type="number" value={(params as any).poolSize} onChange={handleParamChange} /></label>
+                        <label>Strides: <input name='strides' type="number" value={(params as any).strides} onChange={handleParamChange} /></label>
+                        <label>Padding: <select name="padding" value={(params as any).padding} onChange={handleParamChange} /></label>
+                    </>
+                );
+            case 'Dropout':
+                return (
+                    <label>Rate: <input name="rate" type="number" step="0.01" min="0" max="1" value={(params as any).rate} onChange={handleParamChange} /></label>
+                );
+            case 'Dense':
+                return (
+                    <label>Units: <input name="units" type="number" value={(params as any).units} onChange={handleParamChange} /></label>
+                );
+            default:
+                return null;
+        }
+    };
+
+    return (
+        <div>
+            <h3>Add Layer</h3>
+            <select value={layerType} onChange={handleTypeChange}>
+                {Object.keys(defaultParams).map((type) => (
+                    <option key={type} value={type}>{type}</option>
+                ))}
+            </select>
+            {renderParams()}
+            <button onClick={handleAdd}>Add Layer</button>
+        </div>
+    );
+};
+
+export default LayerControls;
